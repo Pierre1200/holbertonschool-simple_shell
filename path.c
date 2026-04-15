fdafe8b78e0d019e68127a7d7013f65c2e6cc541
Hugol4ka <hugolaca12300@gmail.com>
2026-04-15 15:07:05 +0200

    Add find_path function as a placeholder for PATH resolution

---
+++ path.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/**
+ * find_path - placeholder for PATH resolution
+ * @command: command to resolve
+ *
+ * Return: NULL
+ */
+char *find_path(char *command)
+{
+	(void)command;
+	return (NULL);
+}
