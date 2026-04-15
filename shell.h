842c7135da75c05d197e01fa269ef0bb1d9928ec
Pierre1200 <rouvellat.p@icloud.com>
2026-04-15 11:35:45 +0200

    Update shell.h header file with function prototypes and global variables

---
+++ shell.h
@@ -0,0 +1,28 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* --- BIBLIOTHÈQUES --- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+/* --- VARIABLES GLOBALES --- */
+extern char **environ;
+
+/* --- PROTOTYPES DES FONCTIONS --- */
+
+/* parser */
+char *read_line(void);
+char **split_line(char *line);
+
+/* exec */
+int execute_cmd(char **args);
+
+/* utils */
+void free_array(char **args);
+
+#endif
