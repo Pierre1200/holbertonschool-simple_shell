df14d4c3b4f9d2f9954aeb9ed15f6d8b6b955131
Hugol4ka <hugolaca12300@gmail.com>
2026-04-16 14:51:54 +0200

    Add additional includes for struct stat and POSIX support in shell.h

--- shell.h
+++ shell.h
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include<unistd.h>
 #include<string.h>
+#include <sys/stat.h>   /*defines struct stat*/
+#include <sys/types.h>  /*often paired with sys/stat.h*/
+#include <unistd.h>     /*for stat() on POSIX systems*/
 
 int main(int ac, char **av);
 int check_builtin(char *command);
