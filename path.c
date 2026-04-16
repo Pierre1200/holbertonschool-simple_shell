d515bdfed523807d301cbb5ce779b1c6a634f543
Hugol4ka <hugolaca12300@gmail.com>
2026-04-16 14:51:48 +0200

    Implement find_path function for command resolution in PATH

--- path.c
+++ path.c
@@ -8,6 +8,44 @@
  */
 char *find_path(char *command)
 {
-	(void)command;
-	return (NULL);
+	int i = 0;
+	int result;
+	char *path_copy, *token_path, *full_path;
+	struct stat st;
+
+	while (environ[i])
+	{
+		if (strncmp(environ[i], "PATH=", 5) == 0)
+		{
+			/*Copy whithout "PATH="*/
+			path_copy = strdup(environ[i] + 5);
+			break;
+		}
+		i++;
+	}
+	if (path_copy == NULL)
+		return (NULL);
+	token_path = strtok(path_copy, ":");
+	while (token_path != NULL) /*loop for test each folders*/
+	{
+		full_path = malloc(strlen(token_path) + strlen(command) + 2); /*MemoryAlloc*/
+		if (full_path == NULL)
+			return (NULL);
+
+		strcpy(full_path, token_path); /* Build path */
+		strcat(full_path, "/");
+		strcat(full_path, command);
+
+		result = stat(full_path, &st); /*FILE EXISTS ?*/
+		if (result == 0) /*SUCCESS*/
+		{
+			free(full_path);
+			return (full_path);
+		}
+		else /*FAILURE*/
+		{
+			free(full_path);
+			token_path = strtok(NULL, ":");
+		}
+	}
 }
