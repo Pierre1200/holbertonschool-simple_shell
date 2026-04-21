c7acb64e6fbe4a0ce868874874eb02f95685b11e
Hugol4ka <hugolaca12300@gmail.com>
2026-04-21 17:33:35 +0200

    Refactor check_builtin function to streamline environment variable printing

--- builtins.c
+++ builtins.c
@@ -24,30 +24,12 @@ int check_builtin(char **args, char *line, char **envp)
 	/* ENV: Print all environment variables */
 	if (strcmp(args[0], "env") == 0)
 	{
-	for (i = 0; envp[i] != NULL; i++)
-	{
-	write(STDOUT_FILENO, envp[i], strlen(envp[i]));
-	write(STDOUT_FILENO, "\n", 1);
-	}
+		for (i = 0; envp[i] != NULL; i++)
+		{
+			write(STDOUT_FILENO, envp[i], strlen(envp[i]));
+			write(STDOUT_FILENO, "\n", 1);
+		}
 		return (1);
-	}
-	/* CD: Change the current working directory */
-	if (strcmp(args[0], "cd") == 0)
-	{
-		if (args[1] == NULL)
-		{
-			/* No argument: go to HOME directory */
-			chdir(getenv("HOME"));
-		}
-		else
-		{
-			/* Try to go to specified directory and handle errors */
-			if (chdir(args[1]) != 0)
-			{
-				perror("cd");
-			}
-		}
-	return (1);
 	}
 	return (0); /* No built-in found, continue to external commands */
 }
