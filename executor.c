2236a50025d0d339ca515726dcee2cee302e14c7
Pierre1200 <rouvellat.p@icloud.com>
2026-04-21 15:51:08 +0200

    Refactor execute_cmd for improved command validation and error handling

--- executor.c
+++ executor.c
@@ -1,29 +1,27 @@
 #include "shell.h"
 
 /**
- * execute_cmd - Executes the command passed as an argument.
- * @args: The array of words returned by split_line.
- * @argv0: The name of the shell program.
- * @envp: The array of environment variables.
- *
- * Return: 1 to continue the shell loop, 0 to stop.
- */
+* execute_cmd - Executes the command passed as an argument.
+* @args: The array of words returned by split_line.
+* @argv0: The name of the shell program.
+* @envp: The array of environment variables.
+*
+* Return: 1 to continue the shell loop, 0 to stop.
+*/
 int execute_cmd(char **args, char *argv0, char **envp)
 {
 	pid_t child;
 	int status;
+
 	char *command_path = NULL;
-	struct stat st;
 
 	if (args[0] == NULL)
 		return (1);
+
 	if (strchr(args[0], '/') != NULL)
 	{
-		if (stat(args[0], &st) == 0 && S_ISREG(st.st_mode) &&
-		    access(args[0], X_OK) == 0)
-		{
+		if (access(args[0], X_OK) == 0)
 			command_path = strdup(args[0]);
-		}
 	}
 	else
 	{
@@ -34,18 +32,22 @@ int execute_cmd(char **args, char *argv0, char **envp)
 		print_error(argv0, args[0]);
 		return (1);
 	}
+
 	child = fork();
 	if (child == 0)
 	{
-		execve(command_path, args, envp);
-		perror(argv0);
-		free(command_path);
-		_exit(EXIT_FAILURE);
+		if (execve(command_path, args, envp) == -1)
+		{
+			perror(argv0);
+			free(command_path);
+			_exit(127);
+		}
 	}
-	else if (child > 0)
-		wait(&status);
+	else if (child == -1)
+		perror("fork");
 	else
-		perror("Error");
+		wait(&status);
+
 	free(command_path);
 	return (1);
 }
